f9a2ea79ddc88293fc450195fdb197e623a1cfd6
C T Chen <34198336+ctychen@users.noreply.github.com>
2019-10-14 19:01:25 -0700

    In progress testing. TODO: Figure out Jython.

---
+++ MiniLib/src/pi/PiControl.java
@@ -0,0 +1,5 @@
+package pi;
+
+public class PiControl {
+
+}
---
+++ MiniLib/src/pi/PiTest.java
@@ -0,0 +1,60 @@
+package pi;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+
+public class PiTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("<--Pi4J--> GPIO Control Example ... started.");
+
+        // create gpio controller
+        final GpioController gpio = GpioFactory.getInstance();
+
+        // provision gpio pin #01 as an output pin and turn on
+        final GpioPinDigitalOutput a= gpio.provisionDigitalOutputPin(RaspiPin.GPIO_13, "Motor A", PinState.HIGH);
+        final GpioPinDigitalOutput b = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_15, "Motor B", PinState.LOW);
+
+        // set shutdown state for this pin
+        a.setShutdownOptions(true, PinState.LOW);
+        b.setShutdownOptions(true, PinState.LOW);
+
+        System.out.println("--> GPIO state should be: ON");
+
+        Thread.sleep(5000);
+
+        // turn off gpio pin #01
+        a.low();
+        b.low();
+        System.out.println("--> GPIO state should be: OFF");
+
+        Thread.sleep(5000);
+
+        // toggle the current state of gpio pin #01 (should turn on)
+        a.toggle();
+        System.out.println("--> GPIO state should be: ON");
+
+        Thread.sleep(5000);
+
+        // toggle the current state of gpio pin #01  (should turn off)
+        a.toggle();
+        System.out.println("--> GPIO state should be: OFF");
+
+        Thread.sleep(5000);
+
+        // turn on gpio pin #01 for 1 second and then off
+        System.out.println("--> GPIO state should be: ON for only 1 second");
+        a.pulse(1000, true); // set second argument to 'true' use a blocking call
+
+        // stop all GPIO activity/threads by shutting down the GPIO controller
+        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
+        gpio.shutdown();
+
+        System.out.println("Exiting ControlGpioExample");
+    }
+}
