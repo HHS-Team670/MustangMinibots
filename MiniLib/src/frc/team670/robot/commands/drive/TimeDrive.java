e7547736c8c203a4d66b17dacea256d6f51d6216
C T Chen <34198336+ctychen@users.noreply.github.com>
2019-10-17 23:38:59 -0700

    trying to figure out whats going on

--- MiniLib/src/frc/team670/robot/commands/drive/TimeDrive.java
+++ MiniLib/src/frc/team670/robot/commands/drive/TimeDrive.java
@@ -12,51 +12,67 @@ import frc.team670.robot.Robot;
 import frc.team670.robot.utils.Logger;
 
 /**
- * An example command.  You can replace me with your own command.
+ * Drive for a set time
  */
 public class TimeDrive extends Command {
 
-    private double speed, seconds;
-    private int executeCounter;
+	private double speed, seconds;
+	private int executeCounter;
+	private double elapsed;
+	/**
+	 * 
+	 * @param seconds
+	 * @param speed   speed for the drive base: [-1.0, +1.0]
+	 */
+	public TimeDrive(double seconds, double speed) {
+		this.speed = speed;
+		this.seconds = seconds;
+		executeCounter = 0;
+		requires(Robot.driveBase);
+	}
 
-  public TimeDrive(double seconds, double speed) {
-    this.speed = speed;
-    this.seconds = seconds;
-    executeCounter = 0;
-    requires(Robot.driveBase);
-  }
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		//setTimeout(seconds);
+		elapsed = 0;
+		System.out.println("Speed: " + speed + " Time: " + seconds);
+		//Logger.consoleLog("Speed: %s Seconds: %s", speed, seconds);
+	}
 
-  // Called just before this Command runs the first time
-  @Override
-  protected void initialize() {
-    setTimeout(seconds);
-    Logger.consoleLog("Speed: %s Seconds: %s", speed, seconds);
-  }
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		Robot.driveBase.tankDrive(speed, speed);
+		System.out.println(timeSinceInitialized());
+		elapsed = timeSinceInitialized();
+		    try {
+				Thread.sleep((long)(seconds*1000));
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		//Logger.consoleLog();
+	}
 
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  protected void execute() { 
-    Robot.driveBase.tankDrive(speed, speed, false);
-    Logger.consoleLog();
-  }
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return (elapsed == seconds);
+	}
 
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    return isTimedOut();
-  }
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+		Robot.driveBase.stop();
+		System.out.println("Speed: " + speed + " Time: " + seconds);
+		Logger.consoleLog("Speed: %s Seconds: %s", speed, seconds);
+	}
 
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-    Robot.driveBase.stop();
-    Logger.consoleLog("Speed: %s Seconds: %s", speed, seconds);
-  }
-
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
-    end();
-  }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+		end();
+	}
 }
