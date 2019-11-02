a754b022edce6278bad7b50447a3d7824e580679
Laksh Bhambhani <lakshbh@gmail.com>
2019-11-01 17:00:45 -0700

    u

--- MiniLib/src/frc/team670/robot/commands/drive/DistanceDrive.java
+++ MiniLib/src/frc/team670/robot/commands/drive/DistanceDrive.java
@@ -1,58 +1,50 @@
 package frc.team670.robot.commands.drive;
 
 import edu.wpi.first.wpilibj.command.Command;
-import frc.team670.pi.sensors.Encoder;
 import frc.team670.robot.Robot;
 import frc.team670.robot.utils.Logger;
 
-public class DistanceDrive extends Command{
-	private double speed, target, traveled;
-	private Encoder leftEncoder, rightEncoder;
-	private int leftTickCount, rightTickCount;
-
-	public DistanceDrive(double target, double speed) {
-    this.speed = speed;
-    this.target = target;
-    requires(Robot.driveBase);
-	leftEncoder = Robot.driveBase.getLeftEncoder();
-	rightEncoder = Robot.driveBase.getRightEncoder();
-  }
+public class DistanceDrive extends Command {
+	
+	private double speedL, speedR, dist;
+	
+	public DistanceDrive(double distance_cm, double lspeed, double rspeed) {
+		this.speedL = lspeed;
+		this.speedR = rspeed;
+		//this.seconds = seconds;
+		this.dist = distance_cm;
+		requires(Robot.driveBase);
+	}
 
 	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
-		this.traveled = 0;
-		Logger.consoleLog("Speed: %s Distance traveled: %s", speed, traveled);
-	}
+		//setTimeout(seconds);
+		Logger.consoleLog("LeftSpeed: %s Right Speed: %s Distance: %s traveled more than dist: %s", 
+				speedL, speedR, dist, getDistance() > Math.abs(dist));
+	}	
 
 	// Called repeatedly when this Command is scheduled to run
 	@Override
-	protected void execute() {
-		leftTickCount = leftEncoder.getTicks();
-		rightTickCount = rightEncoder.getTicks();
-		this.traveled = leftEncoder.getDistance();
-		if (leftTickCount < rightTickCount) {
-			Robot.driveBase.tankDrive(speed, speed * 0.9, false);
-		} else if (rightTickCount < leftTickCount) {
-			Robot.driveBase.tankDrive(speed * 0.9, speed, false);
-		} else {
-			Robot.driveBase.tankDrive(speed, speed, false);
-		}
-
-		Logger.consoleLog();
+	protected void execute() { 
+		Logger.consoleLog("LeftSpeed: %s Right Speed: %s Distance: %s traveled more than dist: %s", 
+				speedL, speedR, dist, getDistance() > Math.abs(dist));
+		Robot.driveBase.tankDrive(speedL, speedR, false);
+		correct();
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
-		return ((Math.abs(this.traveled - this.target)) < 1);
+		return getDistance() > Math.abs(dist);
 	}
-
+	
 	// Called once after isFinished returns true
 	@Override
 	protected void end() {
 		Robot.driveBase.stop();
-		Logger.consoleLog("Speed: %s Distance traveled: %s", speed, target);
+		Logger.consoleLog("LeftSpeed: %s Right Speed: %s Distance: %s", 
+				speedL, speedR, dist);
 	}
 
 	// Called when another command which requires one or more of the same
@@ -60,5 +52,28 @@ public class DistanceDrive extends Command{
 	@Override
 	protected void interrupted() {
 		end();
+	}	
+	
+	// Checks that the wheels are driving at the same speed, corrects the speed
+	// so that the left/right are equal
+	public void correct() {
+		double currentTicksL = Robot.driveBase.getLeftEncoder().getTicks();
+		double currentTicksR = Robot.driveBase.getRightEncoder().getTicks();
+		
+		if (Math.abs(currentTicksL - currentTicksR) < 5)
+			return;
+		
+		else if (currentTicksL > currentTicksR)
+				speedL -= 0.01;
+		
+		else if (currentTicksL < currentTicksR)
+				speedR -= 0.01;
 	}
+	
+	public double getDistance()
+	{
+		double distance = Robot.driveBase.getLeftEncoder().getDistance();
+		return Math.abs(distance);
+	}
+
 }
