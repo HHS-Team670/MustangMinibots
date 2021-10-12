7a59cfa854f28abe11b802bcb2165e6306303af8
FrancCallahan <91099912+FrancCallahan@users.noreply.github.com>
2021-10-12 16:59:47 -0700

    Update Turn.java

--- Minibot/src/main/java/frc/team670/robot/commands/Turn.java
+++ Minibot/src/main/java/frc/team670/robot/commands/Turn.java
@@ -4,10 +4,23 @@ public class Turn extends InstantCommand{
 
     private DriveBase driveBase;
     private double degrees;
-    private double power;
+    private double leftPower;
+    private double rightPower;
 
     public Turn(DriveBase driveBase, double degrees, double power){
-        ;
+
+        this.driveBase=driveBase;
+        addRequirements(driveBase);
+
+        // positive degrees --> turns right
+        // negative degrees --> turns left
+        int direction = degrees/abs(degrees) == 1 // 1 --> turning right
+        this.rightPower = -1*direction*power
+        this.leftPower = direction*power
+
     }
     
+    public void execute() {
+        ;
+    }
 }
