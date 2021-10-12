de43cc885769faa87ae0b33f1aff3e210fbddf7d
FrancCallahan <91099912+FrancCallahan@users.noreply.github.com>
2021-10-12 15:58:54 -0700

    Update TimeDrive.java

--- Minibot/src/main/java/frc/team670/robot/commands/drive/TimeDrive.java
+++ Minibot/src/main/java/frc/team670/robot/commands/drive/TimeDrive.java
@@ -19,7 +19,7 @@ public class TimeDrive extends WaitCommand {
   private DriveBase driveBase;
   private double power;
 
-  public TimeDrive( DriveBase driveBase, double time, String unit, double power) {
+  public TimeDrive(DriveBase driveBase, double time, String unit, double power) {
     // Param unit: "s" for seconds, "m" for minutes; default unit is seconds
     super(unit=="m"?time*60:time); // super must be called in the first line of the constructor 
     
