a8a858787b571223b7fc54b0f1386f717231c56e
Alex <67526318+ajs256@users.noreply.github.com>
2022-07-19 18:17:23 -0700

    It helps to use the right file name :facepalm:

--- minibotsetup.sh
+++ minibotsetup.sh
@@ -40,7 +40,7 @@ systemctl enable hostapd
 
 DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
 
-cat >> /etc/dhcpd.conf << EOF
+cat >> /etc/dhcpcd.conf << EOF
 
 interface wlan0
     static ip_address=192.168.$BOT_NUMBER.1/24
