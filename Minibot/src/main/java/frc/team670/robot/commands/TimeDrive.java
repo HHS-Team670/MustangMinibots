7b4bda96e8e4eb4ff3cef18dfe84ea253bcbedcb
arghunter <acgo8888@gmail.com>
2022-10-19 16:04:24 -0700

    time drive init

---
+++ Minibot/src/main/java/frc/team670/robot/commands/TimeDrive.java
@@ -0,0 +1,12 @@
+
+package frc.team670.robot.commands.drive;
+
+import java.util.HashMap;
+import java.util.Map;
+import edu.wpi.first.wpilibj2.command.WaitCommand; //https://first.wpi.edu/FRC/roborio/release/docs/java/edu/wpi/first/wpilibj2/command/WaitCommand.html
+import frc.team670.robot.subsystems.DriveBase;
+import frc.team670.robot.utils.Logger;
+
+public class TimeDrive {
+    
+}
